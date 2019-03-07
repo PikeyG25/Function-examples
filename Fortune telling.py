9dc68e65d20fcfd828b4bf646b57e7679b9c0bcf
Parker Gowans <parker.gowans@tooeleschools.org>
2019-03-07 14:06:07 -0700

    Class

---
+++ Fortune telling.py
@@ -0,0 +1,18 @@
+# Parker Gowans
+# 3/19
+#Fortune Telling
+
+def main():
+    num = input("Type a number from 1 to 5. Then click OK or press enter")
+    if num == "1":
+        print("You will get a Dr. Pepper")
+    if num == "2":
+        print("You will break your ankle")
+    if num == "3":
+        print("You will get a new girlfriend.")
+    if num == "4":
+        print("You will get polio.")
+    if num == "5":
+        print("You will ace a test.")
+
+main()
